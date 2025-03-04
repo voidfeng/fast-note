5d0facee0407333e323b77fce7450a4db44f544a
guanzhao <guanzhao>
2025-03-04 10:26:04 +0800

    feat: Add message list and detail views with routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,20 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/home'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      },
-    ]
+    path: '/home',
+    name: 'Home',
+    component: HomePage
   },
   {
-    path: '/note',
-    component: () => import('@/views/NotePage.vue')
+    path: '/n/:id',
+    component: () => import('../views/ViewMessagePage.vue')
   }
 ]
 
