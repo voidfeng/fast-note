4dcaf39b2ef186b432486c968f5e66ff61c36f31
virjay <x@0122.vip>
2025-04-19 14:37:50 +0800

    chore: 添加 vconsole 并更新依赖配置
    添加 vconsole 用于调试，并调整了 package.json 中的依赖顺序。同时更新了 package-lock.json 以反映新的依赖关系。

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
+import { IonicVue } from '@ionic/vue'
+import VConsole from 'vconsole'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { useDexie } from './hooks/useDexie'
 
-import { IonicVue } from '@ionic/vue'
+import router from './router'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css'
-
 /* Basic CSS for apps built with Ionic */
 import '@ionic/vue/css/normalize.css'
 import '@ionic/vue/css/structure.css'
-import '@ionic/vue/css/typography.css'
 
+import '@ionic/vue/css/typography.css'
 /* Optional CSS utils that can be commented out */
 import '@ionic/vue/css/padding.css'
 import '@ionic/vue/css/float-elements.css'
 import '@ionic/vue/css/text-alignment.css'
 import '@ionic/vue/css/text-transformation.css'
 import '@ionic/vue/css/flex-utils.css'
-import '@ionic/vue/css/display.css'
 
 /**
  * Ionic Dark Mode
@@ -27,17 +27,20 @@ import '@ionic/vue/css/display.css'
  * https://ionicframework.com/docs/theming/dark-mode
  */
 
+import '@ionic/vue/css/display.css'
+
 /* @import '@ionic/vue/css/palettes/dark.always.css'; */
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
 import '@ionic/vue/css/palettes/dark.system.css'
-
 /* Theme variables */
 import './theme/variables.css'
+
 import './css/common.scss'
 
 import 'virtual:uno.css'
 
-import { useDexie } from './hooks/useDexie'
+// eslint-disable-next-line no-new
+new VConsole({ theme: 'dark' })
 
 const { init } = useDexie()
 
