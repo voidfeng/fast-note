5d2d756a184742b02a33ac0868aef76ebd5a32a3
virjay <x@0122.vip>
2025-05-12 20:07:09 +0800

    feat: 更新Vite配置以支持环境变量和HTTPS
    - 修改vite.config.ts，使用loadEnv加载环境变量，动态添加HTTPS支持。
    - 优化插件配置，确保在需要时启用basicSsl插件。
    - 保持其他配置项不变，确保项目构建和开发环境的一致性。

--- vite.config.ts
+++ vite.config.ts
@@ -5,40 +5,50 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { injectVersion } from './vite-plugin-inject-version'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, '.')
+
+  const plugins = [
     vue(),
     // legacy()
     UnoCSS(),
     injectVersion(),
-    basicSsl(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    // https: true,
-    proxy: {
-      '/e': {
-        target: 'https://next.0122.vip',
-        changeOrigin: true,
-      },
-      '/d': {
-        target: 'https://next.0122.vip',
-        changeOrigin: true,
+  ]
+
+  if (env.VITE_HTTPS) {
+    plugins.push(basicSsl())
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      // https: true,
+      proxy: {
+        '/e': {
+          target: 'https://next.0122.vip',
+          changeOrigin: true,
+        },
+        '/d': {
+          target: 'https://next.0122.vip',
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
